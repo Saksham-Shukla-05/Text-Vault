61abc1b4021a53eeef85cafab79e00262dd8f601
Saksham-Shukla-05 <get467997@gmail.com>
2025-07-01 16:08:00 +0530

    feat: add form components and button UI, integrate react-hook-form and radix-ui

--- src/app/(auth)/sign-in/page.tsx
+++ src/app/(auth)/sign-in/page.tsx
@@ -1,7 +1,18 @@
-import React from "react";
+"use client";
+import React, { useState } from "react";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import * as z from "zod";
+import { useDebounceValue } from "usehooks-ts";
 
-function page() {
+function SignIn() {
+  const [username, setUsername] = useState("");
+  const [userNameMessage, setuserNameMessage] = useState("");
+  const [isCheckingUsername, setIsCheckingUsername] = useState(false);
+  const [isSubmitting, setIsSubmitting] = useState(false);
+  const debouncedUsername = useDebounceValue(username, 300);
+
   return <div>page</div>;
 }
 
-export default page;
+export default SignIn;
