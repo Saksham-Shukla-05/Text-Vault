1fff9325e4c26556bde19d4a646e52261b62d575
Saksham-Shukla-05 <get467997@gmail.com>
2025-07-03 11:37:17 +0530

    feat: add dashboard, layout, and user components; implement Navbar with authentication handling

--- src/app/(app)/layout.tsx
+++ src/app/(app)/layout.tsx
@@ -1,10 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 
-import AuthProvider from "@/context/AuthProvider";
 import { Toaster } from "@/components/ui/sonner";
-import "./globals.css";
-import Navbar from "@/components/Navbar";
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
@@ -27,15 +24,12 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <AuthProvider>
-        <body
-          className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-        >
-          <Navbar />
-          {children}
-          <Toaster />
-        </body>
-      </AuthProvider>
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+        {children}
+        <Toaster />
+      </body>
     </html>
   );
 }
---
+++ src/app/U/[username]/page.tsx
@@ -0,0 +1,8 @@
+"use client";
+import React from "react";
+
+function User() {
+  return <div>User</div>;
+}
+
+export default User;
