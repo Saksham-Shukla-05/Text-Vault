1b7b2a4e62826dc28350332f110cd74aea303bae
Saksham-Shukla-05 <get467997@gmail.com>
2025-07-03 10:59:06 +0530

    feat: enhance SignIn component with form handling and validation; update email input type for better UX

--- src/app/(auth)/sign-in/page.tsx
+++ src/app/(auth)/sign-in/page.tsx
@@ -1,13 +1,28 @@
+"use client";
+import { Button } from "@/components/ui/button";
+import {
+  Form,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
 import { signInSchema } from "@/schemas/signInSchemas";
 import { zodResolver } from "@hookform/resolvers/zod";
 import axios, { AxiosError } from "axios";
+import { Loader2 } from "lucide-react";
+import { signIn } from "next-auth/react";
+import Link from "next/link";
 import { useRouter } from "next/navigation";
-import React, { useEffect } from "react";
+import React, { useState } from "react";
 import { useForm } from "react-hook-form";
+
+import { toast } from "sonner";
 import { z } from "zod";
 
 function SignIn() {
-  const [identifier, setIdentifier] = useState("");
+  const [Identifier, setIdentifier] = useState("");
   const [password, setPassword] = useState("");
   const [isSubmitting, setIsSubmitting] = useState(false);
   const router = useRouter();
@@ -19,14 +34,81 @@ function SignIn() {
     },
   });
 
-  const onSubmit = (data: z.infer<typeof signInSchema>) => {
-    setIsSubmitting(true);
-    try {
-      const res = await axios.post;
-    } catch (error) {}
+  const onSubmit = async (data: z.infer<typeof signInSchema>) => {
+    //using next-auth for signin
+
+    const res = await signIn("credentials", {
+      Identifier: data.Identifier,
+      password: data.password,
+    });
+
+    if (res?.error) {
+      toast.error("An error occured while log in");
+    }
+
+    if (res?.url) {
+      router.replace("/dashboard");
+    }
   };
 
-  return <div>SignIn</div>;
+  return (
+    <div className="flex justify-center items-center min-h-screen bg-gray-800">
+      <div className="w-full max-w-md p-8 space-y-8 bg-white rounded-lg shadow-md">
+        <div className="text-center">
+          <h1 className="text-4xl font-extrabold tracking-tight lg:text-5xl mb-6">
+            Welcome back
+          </h1>
+          <p className="mb-4">Sign in to start your anonymous adventure</p>
+        </div>
+        <Form {...form}>
+          <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6">
+            <FormField
+              name="Identifier"
+              control={form.control}
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Email</FormLabel>
+                  <Input {...field} name="email" />
+
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+
+            <FormField
+              name="password"
+              control={form.control}
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Password</FormLabel>
+                  <Input type="password" {...field} name="password" />
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <Button type="submit" className="w-full" disabled={isSubmitting}>
+              {isSubmitting ? (
+                <>
+                  <Loader2 className="mr-2 h-4 w-4 animate-spin" />
+                  Please wait
+                </>
+              ) : (
+                "Sign Up"
+              )}
+            </Button>
+          </form>
+        </Form>
+        <div className="text-center mt-4">
+          <p>
+            Don&apos;t have an account ?{" "}
+            <Link href="/sign-up" className="text-blue-600 hover:text-blue-800">
+              Sign up
+            </Link>
+          </p>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default SignIn;
