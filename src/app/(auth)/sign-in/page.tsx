bfe5d8d6b2f1e0570820a10a42e33b4823bba8fe
Saksham-Shukla-05 <get467997@gmail.com>
2025-07-03 10:39:31 +0530

    feat: implement SignIn component with form handling, validation schema, and API integration

--- src/app/(auth)/sign-in/page.tsx
+++ src/app/(auth)/sign-in/page.tsx
@@ -1,6 +1,31 @@
-import React from "react";
+import { signInSchema } from "@/schemas/signInSchemas";
+import { zodResolver } from "@hookform/resolvers/zod";
+import axios, { AxiosError } from "axios";
+import { useRouter } from "next/navigation";
+import React, { useEffect } from "react";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
 
 function SignIn() {
+  const [identifier, setIdentifier] = useState("");
+  const [password, setPassword] = useState("");
+  const [isSubmitting, setIsSubmitting] = useState(false);
+  const router = useRouter();
+  const form = useForm<z.infer<typeof signInSchema>>({
+    resolver: zodResolver(signInSchema),
+    defaultValues: {
+      Identifier: "",
+      password: "",
+    },
+  });
+
+  const onSubmit = (data: z.infer<typeof signInSchema>) => {
+    setIsSubmitting(true);
+    try {
+      const res = await axios.post;
+    } catch (error) {}
+  };
+
   return <div>SignIn</div>;
 }
 
