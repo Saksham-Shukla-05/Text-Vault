61ece8f977ca4ae4338791c83fab8e17ed34930a
Saksham-Shukla-05 <get467997@gmail.com>
2025-07-05 13:31:04 +0530

    feat: refine user authentication and message retrieval logic in API route

--- src/app/api/get-messages/route.ts
+++ src/app/api/get-messages/route.ts
@@ -1,60 +1,55 @@
 import dbConnect from "@/lib/dbConnect";
 import UserModel from "@/model/User";
-import { getSession } from "next-auth/react";
-import { getServerSession, User } from "next-auth";
-import { authOptions } from "../auth/[...nextauth]/options";
 import mongoose from "mongoose";
+import { User } from "next-auth";
+import { getServerSession } from "next-auth/next";
+import { authOptions } from "../auth/[...nextauth]/options";
 
 export async function GET(request: Request) {
   await dbConnect();
   const session = await getServerSession(authOptions);
-  const user: User = session?.user;
+  const _user: User = session?.user;
 
-  if (!session || !session.user) {
+  if (!session || !_user) {
     return Response.json(
-      {
-        success: false,
-        message: "Not Authenticated ",
-      },
+      { success: false, message: "Not authenticated" },
       { status: 401 }
     );
   }
-
-  const userId = new mongoose.Types.ObjectId(user._id); //In  Aggrecation need this
+  const userId = new mongoose.Types.ObjectId(_user._id);
 
   try {
     const user = await UserModel.aggregate([
-      { $match: { id: userId } },
-      { $unwind: "$messages" },
+      { $match: { _id: userId } },
+      { $unwind: { path: "$messages", preserveNullAndEmptyArrays: true } },
       { $sort: { "messages.createdAt": -1 } },
-      { $group: { _id: "$_id", messages: { $push: "$messages" } } },
-    ]);
+      {
+        $group: {
+          _id: "$_id",
+          messages: { $push: "$messages" },
+        },
+      },
+    ]).exec();
+
+    console.log("user here ", user);
 
     if (!user || user.length === 0) {
       return Response.json(
-        {
-          success: false,
-          message: "user not found",
-        },
-        { status: 401 }
+        { message: "User not found", success: false },
+        { status: 404 }
       );
     }
 
     return Response.json(
+      { messages: user[0].messages },
       {
-        success: true,
-        messages: user[0].messages,
-      },
-      { status: 200 }
+        status: 200,
+      }
     );
   } catch (error) {
-    console.log("An Error Occured ", error);
-
+    console.error("An unexpected error occurred:", error);
     return Response.json(
-      {
-        success: false,
-        message: "Error registering user",
-      },
+      { message: "Internal server error", success: false },
       { status: 500 }
     );
   }
